0c723ee77229d179c7aaf50d2d67c34e20a494e9
OTDZ <omartehami6@gmail.com>
2023-05-11 14:37:34 +0100

    Add JPA annotations to Product

--- src/main/java/com/otdz/productspringrestapi/entity/Product.java
+++ src/main/java/com/otdz/productspringrestapi/entity/Product.java
@@ -1,6 +1,15 @@
 package com.otdz.productspringrestapi.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Product {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int id;
     private String name;
     private String description;
