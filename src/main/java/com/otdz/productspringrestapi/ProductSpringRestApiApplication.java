9a52416025cf019c47c511037fafb23de20c238c
OTDZ <omartehami6@gmail.com>
2023-05-11 14:27:13 +0100

    Initial commit

---
+++ src/main/java/com/otdz/productspringrestapi/ProductSpringRestApiApplication.java
@@ -0,0 +1,13 @@
+package com.otdz.productspringrestapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductSpringRestApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductSpringRestApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/otdz/productspringrestapi/ProductSpringRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.otdz.productspringrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductSpringRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
