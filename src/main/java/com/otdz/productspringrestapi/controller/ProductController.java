1fb9786406ee8382d29b6398d922b2295b44c0be
OTDZ <omartehami6@gmail.com>
2023-05-11 15:13:19 +0100

    Create ProductController

---
+++ src/main/java/com/otdz/productspringrestapi/controller/ProductController.java
@@ -0,0 +1,20 @@
+package com.otdz.productspringrestapi.controller;
+
+import com.otdz.productspringrestapi.entity.Product;
+import com.otdz.productspringrestapi.repository.ProductRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class ProductController {
+
+    private ProductRepository productRepository;
+
+    // Dependency injection
+    public ProductController(ProductRepository productRepository){
+        this.productRepository = productRepository;
+    }
+
+}
